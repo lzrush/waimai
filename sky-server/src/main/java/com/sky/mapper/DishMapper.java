4ba0ab21f921643195267644aa2144f1b733428e
lzrush <207015990@qq.com>
2025-10-23 15:51:10 +0800

    删除菜品开发

--- sky-server/src/main/java/com/sky/mapper/DishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishMapper.java
@@ -1,7 +1,12 @@
 package com.sky.mapper;
 
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
+import com.github.pagehelper.Page;
+import com.sky.annotation.AutoFill;
+import com.sky.dto.DishPageQueryDTO;
+import com.sky.entity.Dish;
+import com.sky.enumeration.OperationType;
+import com.sky.vo.DishVO;
+import org.apache.ibatis.annotations.*;
 
 @Mapper
 public interface DishMapper {
@@ -14,4 +19,17 @@ public interface DishMapper {
     @Select("select count(id) from dish where category_id = #{categoryId}")
     Integer countByCategoryId(Long categoryId);
 
+    @AutoFill(value = OperationType.INSERT)
+    @Insert("insert into dish (name, category_id, price, image, description, status, create_time, update_time, create_user, update_user) "
+    +"values" + "(#{name},#{categoryId},#{price},#{image},#{description},#{status},#{createTime},#{updateTime},#{createUser},#{updateUser})")
+    @Options(useGeneratedKeys = true,keyProperty = "id")
+    void insert(Dish dish);
+
+    Page<DishVO> page(DishPageQueryDTO dishPageQueryDTO);
+
+    @Select("select * from dish where id = #{id}")
+    Dish getById(Long id);
+
+    @Delete("delete from dish where id = #{id}")
+    void deleteById(Long id);
 }
