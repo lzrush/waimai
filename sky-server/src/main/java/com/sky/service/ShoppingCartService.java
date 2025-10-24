a9cf5a96caaa8734069da98889b839e350bbfda4
lzrush <207015990@qq.com>
2025-10-24 22:05:00 +0800

    删除购物车中的一个商品

--- sky-server/src/main/java/com/sky/service/ShoppingCartService.java
+++ sky-server/src/main/java/com/sky/service/ShoppingCartService.java
@@ -1,4 +1,16 @@
 package com.sky.service;
 
+import com.sky.dto.ShoppingCartDTO;
+import com.sky.entity.ShoppingCart;
+
+import java.util.List;
+
 public interface ShoppingCartService {
+    void add(ShoppingCartDTO shoppingCartDTO);
+
+    List<ShoppingCart> findAll();
+
+    void deleteBatch();
+
+    void delete(ShoppingCartDTO shoppingCartDTO);
 }
--- sky-server/src/main/java/com/sky/service/impl/ShoppingCartServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/ShoppingCartServiceImpl.java
@@ -1,8 +1,92 @@
 package com.sky.service.impl;
 
+import com.sky.context.BaseContext;
+import com.sky.dto.ShoppingCartDTO;
+import com.sky.entity.Dish;
+import com.sky.entity.Setmeal;
+import com.sky.entity.ShoppingCart;
+import com.sky.mapper.DishMapper;
+import com.sky.mapper.SetmealMapper;
+import com.sky.mapper.ShoppingCartMapper;
 import com.sky.service.ShoppingCartService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 public class ShoppingCartServiceImpl implements ShoppingCartService {
+    @Autowired
+    private ShoppingCartMapper shoppingCartMapper;
+
+    @Autowired
+    private DishMapper dishMapper;
+
+    @Autowired
+    private SetmealMapper setmealMapper;
+
+    @Override
+    public void add(ShoppingCartDTO shoppingCartDTO) {
+        ShoppingCart shoppingCart = new ShoppingCart();
+        BeanUtils.copyProperties(shoppingCartDTO,shoppingCart);
+        shoppingCart.setUserId(BaseContext.getCurrentId());
+
+        List<ShoppingCart> list = shoppingCartMapper.list(shoppingCart);
+
+        if (list != null && list.size() > 0){
+            ShoppingCart cart = list.get(0);
+            cart.setNumber(cart.getNumber() + 1);
+            shoppingCartMapper.updateById(cart);
+        }else{
+            Long dishId = shoppingCartDTO.getDishId();
+            Long setmealId = shoppingCart.getSetmealId();
+
+            if (dishId != null){
+                Dish dish = dishMapper.getById(dishId);
+                shoppingCart.setName(dish.getName());
+                shoppingCart.setImage(dish.getImage());
+                shoppingCart.setAmount(dish.getPrice());
+            }else{
+                Setmeal setmeal = setmealMapper.findById(setmealId);
+                shoppingCart.setName(setmeal.getName());
+                shoppingCart.setImage(setmeal.getImage());
+                shoppingCart.setAmount(setmeal.getPrice());
+            }
+            shoppingCart.setNumber(1);
+            shoppingCart.setCreateTime(LocalDateTime.now());
+            shoppingCartMapper.insert(shoppingCart);
+        }
+
+    }
+
+    @Override
+    public List<ShoppingCart> findAll() {
+        ShoppingCart cart = new ShoppingCart();
+        cart.setUserId(BaseContext.getCurrentId());
+        List<ShoppingCart> list = shoppingCartMapper.list(cart);
+        return list;
+    }
+
+    @Override
+    public void deleteBatch() {
+        ShoppingCart car  = new ShoppingCart();
+        car.setUserId(BaseContext.getCurrentId());
+        shoppingCartMapper.deleteBatch(car);
+    }
+
+    @Override
+    public void delete(ShoppingCartDTO shoppingCartDTO) {
+        ShoppingCart car  = new ShoppingCart();
+        BeanUtils.copyProperties(shoppingCartDTO,car);
+        List<ShoppingCart> list = shoppingCartMapper.list(car);
+        ShoppingCart cart = list.get(0);
+        if(cart.getNumber() == 1) shoppingCartMapper.deleteBatch(cart);
+        else{
+            cart.setNumber(cart.getNumber() - 1);
+            shoppingCartMapper.updateById(cart);
+        }
+
+    }
 }
